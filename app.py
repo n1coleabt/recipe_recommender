b920ce79a671d3bc93a8ee4bbcb0dc6d2556abfe
TorresNA <151115842+n1coleabt@users.noreply.github.com>
2025-03-16 23:34:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if not os.path.exists("faiss_index.idx"):
 def load_faiss_index():
     try:
         index = faiss.read_index("faiss_index.idx")
-        with open("rJPNrecipes.json", "r") as f:
+        with open("rrecipes_metadata.json", "r") as f:
             recipes = json.load(f)
         return index, recipes
     except Exception as e:
